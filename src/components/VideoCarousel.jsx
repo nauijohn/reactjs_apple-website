a9e71ea9e191b876e426f817dbe82053b1ad25ab
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-09-16 23:46:27 +1000

    1:50:36

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -50,14 +50,28 @@ const VideoCarousel = () => {
   const handleLoadedMetadata = (i, e) => setLoadedData((pre) => [...pre, e]);
 
   useEffect(() => {
-    const currentProgress = 0;
+    let currentProgress = 0;
     let span = videoSpanRef.current;
 
     if (span[videoId]) {
       // animate the progress of the video
       let anim = gsap.to(span[videoId], {
-        onUpdate: () => {},
+        onUpdate: () => {
+          const progress = Math.ceil(anim.progress() * 100);
 
+          if (progress != currentProgress) {
+            currentProgress = progress;
+
+            gsap.to(videoDivRef.current[videoId], {
+              width:
+                window.innerWidth < 760
+                  ? "10vw"
+                  : window.innerWidth < 1200
+                  ? "10vw"
+                  : "4vw",
+            });
+          }
+        },
         onComplete: () => {},
       });
     }
